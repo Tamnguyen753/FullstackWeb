76bd31d5721f272e1c0be46610a99c4a1a2f93d7
vudd95 <vu.dd95@gmail.com>
2023-12-28 14:57:19 +0700

    update header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,32 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-import Home from './pages/Home/Home'
-import Schedule from './pages/Schedule/Schedule'
-import Seat from './pages/Seat/Seat'
-import News from './pages/News/News'
-import NotFound from './pages/NotFound/NotFound'
-import Layout from './shared/layout/Layout'
-import Payment from './pages/Payment/Payment'
-
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
+import Schedule from "./pages/Schedule/Schedule";
+import Seat from "./pages/Seat/Seat";
+import News from "./pages/News/News";
+import NotFound from "./pages/NotFound/NotFound";
+import Layout from "./shared/layout/Layout";
+import Payment from "./pages/Payment/Payment";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Layout>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/news' element={<News/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/schedule' element={<Schedule />} />
-      <Route path='/seats' element={<Seat />} />
-      <Route path='/payment' element={<Payment />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-
-    </Layout>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/seats" element={<Seat />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
