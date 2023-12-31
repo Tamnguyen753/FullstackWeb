fd9c9fea2b6a3cd8eb0f10dab2b7f60b26abc46f
Tamnguyen753 <nguyendactam0299@gmail.com>
2023-12-31 17:11:15 +0700

    message2

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,44 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-import Home from './pages/Home/Home'
-import Schedule from './pages/Schedule/Schedule'
-import Seat from './pages/Seat/Seat'
-import News from './pages/News/News'
-import NotFound from './pages/NotFound/NotFound'
-import Layout from './shared/layout/Layout'
-import Payment from './pages/Payment/Payment'
+import React, { createContext, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Home from "./pages/Home/Home";
+import Schedule from "./pages/Schedule/Schedule";
+import Seat from "./pages/Seat/Seat";
+import News from "./pages/News/News";
+import NotFound from "./pages/NotFound/NotFound";
+import Layout from "./shared/layout/Layout";
+import Payment from "./pages/Payment/Payment";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
-
-
-
+export const AppContext = createContext()
 const App = () => {
+  const [user, setUser] = useState(null);
   return (
-    <BrowserRouter>
-    <Layout>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/news' element={<News/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/schedule' element={<Schedule />} />
-      <Route path='/seats' element={<Seat />} />
-      <Route path='/payment' element={<Payment />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/seats" element={<Seat />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+        <ToastContainer />
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
+};
 
-    </Layout>
-    </BrowserRouter>
-
-  )
-}
-
-export default App
+export default App;
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,85 +1,129 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Login.css";
 import { Button } from "antd";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import useAuth from "../../hooks/useAuth";
+import { toast } from "react-toastify";
+import { extractMessageFromErr } from "../../utils/error";
+import Input from "antd/es/input/Input";
+import ErrorsMessage from "../../shared/components/ErrorMessages";
 
 
-const Login = () => { 
-  const [email, setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const navigate = useNavigate();
-  const [error,setError] = useState('');
-  const handleLogin = async () => {
-    if (!email || !password) {
-      setError('Tài khoản và mật khẩu không được bỏ trống !');
-      return;
+// const Login = () => { 
+//   const [email, setEmail] = useState('');
+//   const [password,setPassword] = useState('');
+//   const navigate = useNavigate();
+//   const [error,setError] = useState('');
+//   const handleLogin = async () => {
+//     if (!email || !password) {
+//       setError('Tài khoản và mật khẩu không được bỏ trống !');
+//       return;
+//     }
+//     try {
+//       const response = await axios.post('http://localhost:8000/login', {
+//         email,
+//         password,
+//       });
+//       const token =response.data
+//       console.log(token);
+
+//       localStorage.setItem("access_token",JSON.stringify(token));
+//       alert('Đăng nhập thành công!')
+//       navigate('/');
+//     } catch (error) {
+//       console.error('Có lỗi khi call api')
+//       alert('Sai tài khoản hoặc mật khẩu ')
+//       return
+//       setError('Tài khoản và mật khẩu không được bỏ trống !');
+//     }
+//   }
+  const schema = yup.object().shape({
+    email: yup.string().required(),
+    password:yup.string().required()
+  })
+
+  const Login = () => {
+    const {
+      handleSubmit,
+      control,
+      formState: {errors},
+    } = useForm({
+      resolver: yupResolver(schema),
+    });
+    const {login} = useAuth();
+    const onSubmit = async (data) => {
+      try {
+        login(data) 
+      } catch (err) {
+        toast.error(extractMessageFromErr(err))
+      }
     }
-    try {
-      const response = await axios.post('http://localhost:8000/login', {
-        email,
-        password,
-      });
-      const token =response.data
-      console.log(token);
-
-      localStorage.setItem("access_token",JSON.stringify(token));
-      alert('Đăng nhập thành công!')
-      navigate('/');
-    } catch (error) {
-      console.error('Có lỗi khi call api')
-      alert('Sai tài khoản hoặc mật khẩu ')
-      return
-      // setError('Tài khoản và mật khẩu không được bỏ trống !');
-    }
-  }
   
+
+
+
   return (
     <div
       className="login"
       style={{
         backgroundImage: "url(/picture.png)",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
       }}
     >
       <div className="box-login">
         <div className="content">
           <div className="header-login">
             <h1>Masuk ke TIX ID</h1>
-            <h2>NOMOR HANDPHONE</h2>
           </div>
           <div className="content-login">
-            <input
-              placeholder="Email"
-              type="Email"
-              style={{
-                height: "32px",
-                border: "none",
-                borderBottom: "1px solid gray",
-              }}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <input
+          <div >
+          <h2>NOMOR HANDPHONE</h2>
+            <Controller
+            control={control}
+            name="email"
+            render={({field}) => (
+              <Input 
+              {...field} 
+              status={"error.email" ? "error" : ""}  />)}>
+
+            </Controller>
+            {errors.email && <ErrorsMessage message={errors.email} />}
+ 
+            </div>
+            <div >
+            <h2>Password</h2>
+            <Controller
+            control={control}
+            name="password"
+            render={({field}) => (
+              <Input 
+              {...field} 
+              status={"error.password" ? "error" : ""}
               type="password"
-              placeholder="Password"
-              style={{
-                height: "32px",
-                border: "none",
-                borderBottom: "1px solid gray",
-              }}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-            <Button type="primary" onClick={handleLogin}>Sign in</Button>
+              />)}>
+
+            </Controller>
+            {errors.password && <ErrorsMessage message={errors.password} />}
+            </div>
+          </div>
+          <div className="btn">
+          <Button
+        className="submit"
+        type="primary"
+        onClick={handleSubmit(onSubmit)}
+
+      >
+        Sign in
+      </Button>
             <Link to="/register">
             <Button className="btn-signup">Sign up</Button>
             </Link>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
+          </div>
           </div>
         </div>
       </div>
-    </div>
   );
-};
-
+          }
 export default Login;
